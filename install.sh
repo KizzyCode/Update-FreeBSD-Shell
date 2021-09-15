1a6ef0b10dc1faea439dc8cffa883c1e0ef94a0b
Keziah Biermann <development@kizzycode.de>
2021-09-15 02:34:21 +0200

    Use hookworm to process the hooks

--- install.sh
+++ install.sh
@@ -9,12 +9,10 @@ echo "Creating entries..."
 
 # Create update.d directory
 install -v -d "$PREFIX/sbin"
-install -v -d "$PREFIX/libexec"
 install -v -d "$PREFIX/libexec/update.d"
 
 # Install scripts
 install -v -m 0755 "sbin/update" "$PREFIX/sbin/update"
-install -v -m 0755 "libexec/update" "$PREFIX/libexec/update"
 install -v -m 0755 "libexec/update.d/1-freebsd" "$PREFIX/libexec/update.d/1-freebsd"
 install -v -m 0755 "libexec/update.d/2-freebsd-packages" "$PREFIX/libexec/update.d/2-freebsd-packages"
 install -v -m 0755 "libexec/update.d/3-freebsd-ports" "$PREFIX/libexec/update.d/3-freebsd-ports"
--- uninstall.sh
+++ uninstall.sh
@@ -9,7 +9,6 @@ echo "Deleting entries..."
 
 # Install scripts
 rm -v -f "$PREFIX/sbin/update"
-rm -v -f "$PREFIX/libexec/update"
 rm -v -f "$PREFIX/libexec/update.d/1-freebsd"
 rm -v -f "$PREFIX/libexec/update.d/2-freebsd-packages"
 rm -v -f "$PREFIX/libexec/update.d/3-freebsd-ports"
