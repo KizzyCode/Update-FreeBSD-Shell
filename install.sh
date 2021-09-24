020bad796c59ac3f42be469f8bedee05b12576d8
Keziah Biermann <development@kizzycode.de>
2021-09-25 00:46:47 +0200

    Add support for jails

--- install.sh
+++ install.sh
@@ -13,6 +13,10 @@ install -v -d "$PREFIX/libexec/update.d"
 
 # Install scripts
 install -v -m 0755 "sbin/update" "$PREFIX/sbin/update"
-install -v -m 0755 "libexec/update.d/1-freebsd" "$PREFIX/libexec/update.d/1-freebsd"
-install -v -m 0755 "libexec/update.d/2-freebsd-packages" "$PREFIX/libexec/update.d/2-freebsd-packages"
-install -v -m 0755 "libexec/update.d/3-freebsd-ports" "$PREFIX/libexec/update.d/3-freebsd-ports"
+install -v -m 0755 "libexec/update.d/001-freebsd" "$PREFIX/libexec/update.d/001-freebsd"
+install -v -m 0755 "libexec/update.d/002-freebsd-packages" "$PREFIX/libexec/update.d/002-freebsd-packages"
+install -v -m 0755 "libexec/update.d/003-freebsd-ports" "$PREFIX/libexec/update.d/003-freebsd-ports"
+install -v -m 0755 "libexec/update.d/011-jails" "$PREFIX/libexec/update.d/011-jails"
+install -v -m 0755 "libexec/update.d/012-jails-packages" "$PREFIX/libexec/update.d/012-jails-packages"
+install -v -m 0755 "libexec/update.d/013-jails-ports" "$PREFIX/libexec/update.d/013-jails-ports"
+install -v -m 0755 "libexec/update.d/014-jails-restart" "$PREFIX/libexec/update.d/014-jails-restart"
--- uninstall.sh
+++ uninstall.sh
@@ -7,11 +7,15 @@ PREFIX="${PREFIX:-/usr/local}"
 # Print status info
 echo "Deleting entries..."
 
-# Install scripts
+# Uninstall scripts
 rm -v -f "$PREFIX/sbin/update"
-rm -v -f "$PREFIX/libexec/update.d/1-freebsd"
-rm -v -f "$PREFIX/libexec/update.d/2-freebsd-packages"
-rm -v -f "$PREFIX/libexec/update.d/3-freebsd-ports"
+rm -v -f "$PREFIX/libexec/update.d/001-freebsd"
+rm -v -f "$PREFIX/libexec/update.d/002-freebsd-packages"
+rm -v -f "$PREFIX/libexec/update.d/003-freebsd-ports"
+rm -v -f "$PREFIX/libexec/update.d/011-jails"
+rm -v -f "$PREFIX/libexec/update.d/012-jails-packages"
+rm -v -f "$PREFIX/libexec/update.d/013-jails-ports"
+rm -v -f "$PREFIX/libexec/update.d/014-jails-restart"
 
 # Remove update.d
 rm -v -r -f "$PREFIX/libexec/update.d"
